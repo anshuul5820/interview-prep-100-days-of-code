bd8d73c84286f109ce94acc89d7e22db41631209
Ansul Sah <anshulsah505@gmail.com>
2024-05-22 01:39:53 +0530

    added progress for day 3

--- sorting/src/InsertionSort.java
+++ sorting/src/InsertionSort.java
@@ -1,3 +1,25 @@
 public class InsertionSort {
 
+    public static void insertionSort(int[] arr, int n){
+        for(int i = 1; i < n-1; i++){
+            int j=i;
+            while(j>0 && arr[j]<arr[j-1]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 6, 4};
+
+        insertionSort(arr, arr.length - 1);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+    }
 }
--- sorting/src/MergeSort.java
+++ sorting/src/MergeSort.java
@@ -1,9 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MergeSort {
-    public void merge() {
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        List<Integer> temp = new ArrayList<>();
+        int l = left, r = mid + 1;
+
+        while (l <= mid && r <= right) {
+            if (arr[l] <= arr[r]) {
+                temp.add(arr[l++]);
+            } else {
+                temp.add(arr[r++]);
+            }
+        }
+
+        while (l <= mid) {
+            temp.add(arr[l++]);
+        }
+
+        while (r <= right) {
+            temp.add(arr[r++]);
+        }
+
+        for (int k = left; k <= right; k++) {
+            arr[k] = temp.get(k - left);
+        }
     }
 
-    public void mergeSort() {
-    }
-    
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) return;
 
+        int mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 6, 4};
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
 }
